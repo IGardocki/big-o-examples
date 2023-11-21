9de0691c8328f9a0e01c88e6cb60f4287fd1d0c0
Ian Gardocki <igardocki@kr.af.mil>
2023-11-20 18:45:47 -0600

    added on example

--- o1.c
+++ o1.c
@@ -4,5 +4,6 @@ int main() {
     char cat1[6] = "Romad";
     char cat2[5] = "Mimi";
     printf("I have two cats:\n %s and %s!\n", cat1, cat2);
+    // if we had function that accepted n and returned n*2, also would be o1
     return 0;
 }
